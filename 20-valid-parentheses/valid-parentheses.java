391dd2b390faffdc8d45092dac030d7c5762b7f4
Shreyassh290 <shreyassh354@gmail.com>
2026-07-28 20:09:58 +0530

    Time: 3 ms (86.21%) | Memory: 43.4 MB (26.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
         int n=s.length();
+        if(n%2!=0){
+            return false;
+        }
         for(int i=0;i<n;i++){
             char c=s.charAt(i);
             if(c=='(' || c=='{' || c=='['){
